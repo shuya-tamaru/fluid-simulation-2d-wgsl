d9f60ec05d277f7e924894c8de04b3055bd5a76d
shuya-tamaru <tamaru1013@gmail.com>
2025-09-04 00:48:07 +0900

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { string } from "rollup-plugin-string";
 
 export default defineConfig({
+  base: "/voronoi-myakumyaku-wgsl/",
   plugins: [
     string({
       include: "**/*.wgsl",
